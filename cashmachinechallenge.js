9aec4801cc643edb341c199d395dc5009d72b61c
Shaun Wright <71137882+wrighty1988@users.noreply.github.com>
2020-10-06 13:48:19 +0100

    Add files via upload

---
+++ cashmachinechallenge.js
@@ -0,0 +1,96 @@
+let pinNumber = 1234;//hardcoded
+let pinNumber2= 2345;//hardcoded
+let bankBalance = (1000);
+let withdrawlAmount =(200);
+let remainingBankBalance=(bankBalance-withdrawlAmount);
+let receipt=true;
+let deposit = (250);
+let Transaction=(withdrawlAmount,bankBalance,deposit,bankBalance,receipt,"pinchange",bankBalance)
+
+
+const pinValidation =(pinNumber)=>{{
+    console.log("Please enter Pin")
+
+    if (pinNumber==1234)
+        console.log("Natwest Welcome`s you to your bankaccount")//* If pin is correct then display if and if not correct display else if.
+
+    else if (pinNumber = (1235)) {
+        console.log("Pin incorrect") 
+    }
+
+    else if(pinNumber = 1235){
+        console.log("Pin Incorrect, Last try ")}
+
+    else {
+        console.log("You`re Natwest Card has now been confiscated")
+        ~
+        console.log("~")
+    }
+        console.log("Please choose the service you would like");//* after accepting pin ask what service
+    }
+    
+    if (withdrawlAmount=(`£200`))//*withdraw selected amount and display remaining bank balance 
+
+    if (bankBalance<=withdrawlAmount)//* check if enough money in the bank 
+        console.log("no,")
+
+    else if (bankBalance>withdrawlAmount);{
+        console.log("money being processed")
+    }
+    if ("money being processed") {
+        console.log(`you have withdrawn £${200} from your account.`);
+        console.log(`You have £${remainingBankBalance} left in your account.`)
+    }
+    if (deposit)//*make a deposit of 250 and then add it to the remaining balance
+        console.log("money has been added to your account")
+        console.log(`your new balance is £${deposit+remainingBankBalance}`)
+
+    if (receipt==true)//*give customer recipt 
+        console.log("receipt has been printed ")
+
+   //*change the pin with validation 
+function pinValidation2(pinNumber2){
+console.log(pinValidation2==pinNumber2);
+}
+if (pinValidation2==pinNumber2);{
+console.log("pin number succesfully changed")
+}
+  //* Check balance and cry at the amount. (tears optional)
+bankBalance
+if (bankBalance);{
+console.log("just getting that information for you now "),
+console.log(`you're remainging balance is £${remainingBankBalance}`)
+console.log("~")
+
+console.log("STATEMENT")
+//*Statement of actions of the transactions occurred
+
+let Transaction=(withdrawlAmount,bankBalance,deposit,bankBalance,receipt,"pinchange",bankBalance)
+if (Transaction);{
+console.log(`withdrawl,
+~
+your bankbalance = £${remainingBankBalance},
+~
+a deposit of £${deposit} was made on,
+~
+Bank balance of £${deposit + remainingBankBalance},
+~
+receipt,
+~
+pinchange,
+~
+bankBalance£ ${deposit + remainingBankBalance}`)
+
+console.log("~")
+/*Exchange currency - you can pick your currency. If a user
+wants to exchange £100 and the conversion is 1.5, then they’d
+get 150 of another currency, for example.*/
+
+
+}
+}
+}   
+pinValidation(1234)
+
+
+
